1f5c08ccbf93881097b0bb5911f4edff0d6d92c0
Amos .O. Odundo <114424543+Amos202-create@users.noreply.github.com>
2023-04-06 20:51:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var type = new Typed(".input", {
+    strings:["Amos Odundo.","Web Developer.","UX Designer","Network Engineer."],
+            typeSpeed: 70,
+            backSpeed:60,
+            loop:true
+})
